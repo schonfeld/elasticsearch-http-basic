5836179791f8bf00385381829f72a04369149930
Michael Schonfeld <michael@schonfeld.org>
2014-11-14 15:39:48 -0500

    add jar

--- reinstall.sh
+++
@@ -1,6 +0,0 @@
-ES=/usr/share/elasticsearch
-sudo $ES/bin/plugin remove http-basic
-mvn -DskipTests clean package
-FILE=`ls ./target/elasticsearch-*zip`
-sudo $ES/bin/plugin -url file:$FILE -install http-basic
-sudo service elasticsearch restart
